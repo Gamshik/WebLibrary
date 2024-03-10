c6e6116c13b4277985613b3fb5163394b58a9eeb
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-03-10 03:49:29 +0300

    Create maps for book entities

---
+++ WebApi/WebApi/Profiles/BookPreviewProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.Classes.DTOs.BookDTOs;
+using Domain.Classes.Entities;
+
+namespace WebApi.Profiles
+{
+    public class BookPreviewProfile : Profile
+    {
+        public BookPreviewProfile()
+        {
+            CreateMap<BookPreviewCreateDto, BookPreview>().ForMember("Cover", opt => opt.MapFrom(b => Convert.FromBase64String(b.Cover)));
+            CreateMap<BookPreview, BookPreviewDto>();
+        }
+    }
+}
---
+++ WebApi/WebApi/Profiles/TextOfBookProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Classes.DTOs.BookDTOs;
+using Domain.Classes.Entities;
+
+namespace WebApi.Profiles
+{
+    public class TextOfBookProfile : Profile
+    {
+        public TextOfBookProfile()
+        {
+            CreateMap<TextOfBookCreateDto, TextOfBook>();
+        }
+    }
+}
