8b8ccf83198913867e858d2161ab6ed8ab2d685a
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-03-10 20:52:18 +0300

    Update BookPreviewDto class

--- WebApi/Domain/Classes/DTOs/BookDTOs/BookPreviewDto.cs
+++ WebApi/Domain/Classes/DTOs/BookDTOs/BookPreviewDto.cs
@@ -2,8 +2,6 @@
 {
     public class BookPreviewDto
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
         public byte[] Cover { get; set; }
         public string Author { get; set; }
         public string? Description { get; set; }
