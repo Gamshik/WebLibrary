68ab21999151a1b7c557e21bab67e4f3192b1fd2
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-03-10 03:48:11 +0300

    Create migrations

---
+++ WebApi/WebApi/Migrations/20240309155156_TablesForBookConfigure.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TablesForBookConfigure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BooksPreviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Cover = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BooksPreviews", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TextsOfBooks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TextsOfBooks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TextsOfBooks_BooksPreviews_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BooksPreviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BooksPreviews_Title",
+                table: "BooksPreviews",
+                column: "Title",
+                unique: true,
+                filter: "[Title] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TextsOfBooks_BookId",
+                table: "TextsOfBooks",
+                column: "BookId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TextsOfBooks");
+
+            migrationBuilder.DropTable(
+                name: "BooksPreviews");
+        }
+    }
+}
