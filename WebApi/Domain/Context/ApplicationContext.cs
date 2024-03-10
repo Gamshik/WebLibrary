bc3e74ecf0867ebdb8358b5bf1b24509cff7a5c4
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-03-10 03:47:43 +0300

    Configuring tables to work with books

--- WebApi/Domain/Context/ApplicationContext.cs
+++ WebApi/Domain/Context/ApplicationContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Domain.Classes.Entities;
+using Domain.Context.Configurations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Context
@@ -6,12 +8,17 @@ namespace Domain.Context
     public class ApplicationContext : DbContext
     {
         public DbSet<IdentityUser<int>> Users { get; set; }
+        public DbSet<BookPreview> BooksPreviews { get; set; }
+        public DbSet<TextOfBook> TextsOfBooks { get; set; }
         public ApplicationContext(DbContextOptions<ApplicationContext> builder) : base(builder)
         {
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder); 
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new BooksPreviewsConfiguration());
+            modelBuilder.ApplyConfiguration(new TextsOfBooksConfiguration());
         }
     }
 }
---
+++ WebApi/Domain/Context/Configurations/TextsOfBooksConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Classes.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Domain.Context.Configurations
+{
+    public class TextsOfBooksConfiguration : IEntityTypeConfiguration<TextOfBook>
+    {
+        public void Configure(EntityTypeBuilder<TextOfBook> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Id).ValueGeneratedOnAdd();
+            builder.Property(t => t.Text).IsRequired();
+
+            builder.HasOne(t => t.Book)
+                .WithOne(b => b.TextOfBook)
+                .HasForeignKey<TextOfBook>(t => t.BookId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
